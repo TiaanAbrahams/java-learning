f92a6201725d52562cc16172a84b07e07be91697
Tiaan Abrahams <tjabrahams@hotmail.com>
2026-09-19 21:58:31 +0200

    interface completed

--- ISaveableInterfaceChallenge/src/Main.java
+++ ISaveableInterfaceChallenge/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] Args){
-        System.out.println("Hello worlds");
+
+        List<String> array = new ArrayList<>();
+        array.add("Tiaan");
+        array.add(" is ");
+        array.add(" awesome");
+    }
+    public static List<String> printArray(){
+
     }
 }
